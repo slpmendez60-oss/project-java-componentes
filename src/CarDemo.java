1fc5e16a40e41292321093ad9c1dd6d47b725ec5
Luis Mendez <slpmendez60@gmail.com>
2025-11-13 21:56:16 -0500

    implementación de clase Vehiculo con atributos y métodos

---
+++ src/CarDemo.java
@@ -0,0 +1,10 @@
+public class CarDemo {
+    public static void main(String[] args) {
+// We'll add code here in the next step
+Car myCar = new Car("Toyota", "Corolla", 2022);
+myCar.displayInfo();
+
+Car friendsCar = new Car("Honda", "Civic", 2023);
+friendsCar.displayInfo();
+    }
+}
